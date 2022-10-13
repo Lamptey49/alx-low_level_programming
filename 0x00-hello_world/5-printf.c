f6b5959e10f235120c07027fa5b43c27139eb1e8
Phinhas Lamptey <lampteyphinehas70@gmail.com>
2022-10-13 08:16:58 -0700

    mesage

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/*
+ * main - beginning of the program
+ * 
+ * return - always return (0)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
+	
 	return (0);
 }
