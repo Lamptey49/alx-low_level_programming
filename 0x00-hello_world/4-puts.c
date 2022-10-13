8ffd175b7ebb9211425d64a653ba76b26ec03a47
Phinhas Lamptey <lampteyphinehas70@gmail.com>
2022-10-13 07:54:16 -0700

    First commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char word[100];
+	strcpy(word, "Programming is like building a multilingual puzzle");
+	puts(word);
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+Coding result here
