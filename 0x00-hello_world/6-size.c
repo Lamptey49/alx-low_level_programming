d4e34acbd40afe328e48d1ddf8f1f63bc628860a
Phinhas Lamptey <lampteyphinehas70@gmail.com>
2022-10-14 04:06:14 -0700

    Message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %2d bytes", sizeof(char));
-	printf("Size of an int: %2d bytes", sizeof(short int));
-	printf("Size of a long int: %2d bytes", sizeof(long int));
-	printf("Size of a long long int: %2d bytes", sizeof(unsigned long));
-	printf("Size of a float: %2d bytes", sizeof(float));
+	printf("Size of a char: %2d bytes \n", sizeof(char));
+	printf("Size of an int: %2d bytes \n", sizeof(short int));
+	printf("Size of a long int: %2d bytes \n", sizeof(long int));
+	printf("Size of a long long int: %2d bytes \n", sizeof(unsigned long));
+	printf("Size of a float: %2d bytes \n", sizeof(float));
 
 	return (0);
 }
