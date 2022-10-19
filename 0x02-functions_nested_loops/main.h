784a255b77b56157f2927d3bda454c40978e0885
Phinhas Lamptey <lampteyphinehas70@gmail.com>
2022-10-19 03:27:12 -0700

    functions and nested loops in c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _putchar: a function that prints the word _putchar
+ *
+ * return always 0 (success)
+ */
+void _putchar(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
