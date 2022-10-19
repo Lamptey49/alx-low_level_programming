4f5f9f2118fbde01722971461f0dbbe0cea367fa
Phinhas Lamptey <lampteyphinehas70@gmail.com>
2022-10-19 04:05:21 -0700

    functions and nested loops in c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,6 +9,17 @@
  * Return: Always 0 (Success)
  *
  */
+void print_alphabet()
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+                putchar(c);
+        }
+        putchar('\n');
+}
+
 
 int main(void)
 {
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,13 +12,4 @@ void print_putchar()
 	printf("_putchar\n");
 
 }
-void print_alphabet()
-{
-        char c;
-
-        for (c = 'a'; c <= 'z'; c++)
-        {
-                putchar(c);
-        }
-	putchar('\n');
-}
+void print_alphabet(void);
