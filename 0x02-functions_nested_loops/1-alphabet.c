e70a26a3138f92b5e007d72eac7872c233259b8c
Phinhas Lamptey <lampteyphinehas70@gmail.com>
2022-10-20 02:19:46 -0700

    Functions and nested loop functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,6 +9,17 @@
  * Return: Always 0 (Success)
  *
  */
+void print_alphabet(void)
+{
+	char al;
+
+	for (lal = 'a'; al <= 'z'; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+}
+
 int main(void)
 {
 	print_alphabet();
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * print_putchar: a function that prints the word _putchar
+#ifndef MAIN_H
+#define MAIN_H
+
+/**_putchar: a function that prints the word _putchar
  *
  * print_alphabet: a function that prints alphabets
  *
@@ -9,3 +9,15 @@
  */
 void print_putchar(void);
 void print_alphabet(void);
+void print_alphabet_x10(10);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+
+#endif
