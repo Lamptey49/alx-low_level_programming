835de5a82684b82d41ad72f85d3140d31a787924
Phinhas Lamptey <lampteyphinehas70@gmail.com>
2022-10-20 06:03:36 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * main - function that test either a number is positive or negative
+ * 
  * Return: always 0
  */
 int main(void)
@@ -8,6 +9,7 @@ int main(void)
 	int i;
 
 	i = 0;
+	
 	positive_or_negative(i);
 
 	return (0);
