a890ad5e6b0bb075a0a644317ee9fa0a169ad226
Phinhas Lamptey <lampteyphinehas70@gmail.com>
2022-10-20 05:33:40 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - function that test either a number is positive or negative
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - causes an infinite loop
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = o;
+	/**
+	 * while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
