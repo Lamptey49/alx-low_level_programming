c3f7cc71ccf84c6ef5cdaa182687c0acd3329c19
Phinhas Lamptey <lampteyphinehas70@gmail.com>
2022-10-20 05:40:47 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,19 +14,19 @@ int largest_number(int a, int b, int c)
 	if (a > b)
 	{
 		if (b > c)
-			largest = a;
+			large = a;
 		else if (a > c)
-			largest = a;
+			large = a;
 		else
-			largest = c;
+			large = c;
 	}
 	else
 	{
 		if (b < c)
-			largest = c;
+			large = c;
 		else
-			largest = b;
+			large = b;
 	}
-	return (largest);
+	return (large);
 }
 
